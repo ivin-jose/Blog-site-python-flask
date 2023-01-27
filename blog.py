fb03d0f2824a9727283546bd16caaf3db70bf007
ivin jose <ivinnjose@gmail.com>
2023-01-27 22:42:27 +0530

    custom error pages

--- blog.py
+++ blog.py
@@ -16,3 +16,20 @@ def user(name, id):
 	num = 35
 	return "<h2>Hello {} your id is {}!</h2>".format(name, num)
 
+
+#Custom error pages
+
+#Invalid pages
+
+@app.errorhandler(404)
+
+def page_not_found(e):
+	return render_template('errors/404.html')
+
+#Internal server error
+
+@app.errorhandler(500)
+
+def page_not_found(e):
+	return render_template('500.html')
+
